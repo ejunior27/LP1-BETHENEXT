426494b5c7fdfe34f992285c0170e416d1049c71
ejunior27 <51997209+ejunior27@users.noreply.github.com>
2022-12-01 10:27:23 -0300

    atualização

---
+++ LP1.Aula5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LP1.Aula5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Estruturas condicionais
+
+            //IF/ELSE
+
+
+            //SWITCH/CASE
+            
+        }
+    }
+}
